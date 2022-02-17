b0fbbf65e4a218e945a797c706deef23bfa5c709
Josh Dronfield <josh.dronfield.com>
2022-02-17 18:23:46 +0000

    added image and some text to the about page

--- my-app/src/pages/About.tsx
+++ my-app/src/pages/About.tsx
@@ -2,7 +2,20 @@ import React from 'react'
 
 function About() {
   return (
-    <div>About</div>
+    <div className="container">
+      <div className="left-cont">
+      <h1>About Me</h1>
+        <p>Hello there, my name is Joshua Dronfield and I am a digital tech consultant apprentice at Capita. along side this apprentiship I am studying for a degree in IT consulting.</p>
+        <p>Welcome to my developer portfolio website where I can show off some of the skill I am learning in this degree apprenteship.</p>
+        <p>so far I have been learning how to create responsive websites using javascript frameworks. Such as, React and Typescript</p>
+        <p>if you would like to contact me with any work opportunities or you are a member of Capita viewing my website please fill in this form</p>
+      </div>
+      <div className="right-cont">
+        {/* img */}
+      </div>
+
+      {/*form goes here eg// <form /> component*/}
+    </div>
   )
 }
 
--- my-app/src/pages/Home.tsx
+++ my-app/src/pages/Home.tsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom';
+import portrait from '../img/portrait.png'
 
 function Home() {
+  const navigate = useNavigate();
   return (
-    <div>Home</div>
+  
+    <div className="container">
+      <div className="text-container">
+        <h1 className='hello'>HELLO</h1>
+        <p className='home-text'>My Name is Josh Dronfield. Welcome to my developer portfolio website!</p>
+        <p className='home-text'>click below to learn more</p>
+        <button onClick = {() => navigate('/about') } className = 'button' >learn more</button>
+      </div>
+        <img alt='portrait of Josh' className="portrait" src={String(portrait)} />
+    </div>
   )
 }
 
