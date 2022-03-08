a2f02d2bd95dd115cf3f7301e8df6021dc8b9bec
Josh Dronfield <josh.dronfield.com>
2022-03-08 12:29:19 +0000

    another commit

--- my-app/src/pages/Work.tsx
+++ my-app/src/pages/Work.tsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import { Typography, AppBar, Card, CardActions, CardContent, CardMedia, CssBaseline, Grid, Toolbar, Container } from '@material-ui/core'
+import PhotoCamera from '@material-ui/icons'
 
 function Work() {
   return (
-    <div>Work</div>
-  )
+    <>
+      <CssBaseline />
+      <main>
+        <div>
+          <Container maxWidth = "sm">
+            <Typography variant="h2" align="center" gutterBottom>
+              My Work and experience
+            </Typography>
+          </Container>
+        </div>
+      </main>
+    </>
+  );
 }
 
 export default Work
