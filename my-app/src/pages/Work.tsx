600ebc20d598ac8f5b8857d060063829b082298c
Josh Dronfield <josh.dronfield.com>
2022-03-18 10:20:24 +0000

    navbar responsive

--- my-app/src/pages/Work.tsx
+++ my-app/src/pages/Work.tsx
@@ -1,17 +1,16 @@
 import React from 'react'
 import { Typography, AppBar, Card, CardActions, CardContent, CardMedia, CssBaseline, Grid, Toolbar, Container } from '@material-ui/core'
 import PhotoCamera from '@material-ui/icons'
+import { textAlign } from '@mui/system';
+
 
 function Work() {
   return (
     <>
-      <CssBaseline />
       <main>
         <div>
           <Container maxWidth = "sm">
-            <Typography variant="h2" align="center" gutterBottom>
-              My Work and experience
-            </Typography>
+              <h1 className = 'title'>My work Experience</h1>  
           </Container>
         </div>
       </main>
